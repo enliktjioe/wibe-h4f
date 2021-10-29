44506de574ce173e11a1f813856fe850b809fd12
Enlik <enlik@ut.ee>
2021-10-29 15:10:44 +0300

    test deployment update via Netlify

--- landing-page/README.md
+++ landing-page/README.md
@@ -15,3 +15,13 @@ Run in Production:
 npm run build
 ```
 
+### Deployment via Netlify.app
+
+- On deployment setting use this
+```
+CI=false npm run build
+```
+
+- References:
+ - https://stackoverflow.com/questions/62415804/how-to-prevent-netlify-from-treating-warnings-as-errors-because-process-env-ci
+ - https://dev.to/kapi1/solved-treating-warnings-as-errors-because-of-process-env-ci-true-bk5
--- landing-page/src/components/Footer/Footer.js
+++ landing-page/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer class="page-footer font-small blue footer">
 
-            <div class="footer-copyright text-center py-3">Made with ❤ in
+            <div class="footer-copyright text-center py-3">Made in
         <a href="https://futureforum.ee/uritus/hack4future/" target="_blank" rel="noopener noreferrer" className="author"> Hack4Future Ulemiste City</a>
             </div>
 
